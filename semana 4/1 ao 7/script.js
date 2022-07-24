e4c6094c29004b88bcb1e4a681331c4643a3cd5f
Marcelo Leão <marcelo.lecampos@gmail.com>
2022-07-24 09:57:59 -0300

    Add func de cálculos bancários e ajustes html/css

--- semana 4/1 ao 7/script.js
+++ semana 4/1 ao 7/script.js
@@ -1,28 +1,82 @@
+// 🧲Selectores DOM:
+const body = document.querySelector("body");
+const formulario = document.querySelector("#form");
+const select = document.querySelector("#contas");
+const inputValor = document.querySelector("#valor");
+const operacao = document.querySelector("#operacao");
 
-const arrayContas = [
-    {   
-        id: 1,
-        titular: 'Josh',
-        saldo: 3456
-    },
-    {
-        id: 2,
-        titular: 'Teddy',
-        saldo: 1324
-    },
-    {
-        id: 3,
-        titular: 'Camilla',
-        saldo: 5789
-    }
-]
+// 🧾 Dados dos usuários:
+let arrayContas = [
+  {
+    id: 1,
+    titular: "Josh",
+    saldo: 3456,
+  },
+  {
+    id: 2,
+    titular: "Teddy",
+    saldo: 1324,
+  },
+  {
+    id: 3,
+    titular: "Camilla",
+    saldo: 5789,
+  },
+];
 
+// 🏹 Adiciona o array de contas no select:
 const adicionarContas = () => {
-    const select = document.querySelector('#contas')
-    arrayContas.forEach(conta => {
-        const option = document.createElement('option')
-        option.value = conta.id
-        option.innerHTML = conta.titular
-        select.appendChild(option)
-    } )
-}
+  arrayContas.forEach((conta) => {
+    const option = document.createElement("option");
+    option.value = conta.titular;
+    option.saldo = conta.saldo;
+    option.innerHTML = conta.titular;
+    select.appendChild(option);
+  });
+};
+body.onload = adicionarContas;
+
+// 💽 Funções:
+
+let validarValor = (valor, i) => {
+  if (valor <= 0) {
+    console.log("Valor inválido");
+    return false;
+  } else if (valor > arrayContas[i].saldo) {
+    console.log("Saldo insuficiente");
+    return false;
+  } else if (valor <= arrayContas[i].saldo) {
+    console.log("sucesso");
+    console.log(arrayContas[i].saldo);
+    return true;
+  }
+};
+
+const atualizarForm = (formulario.onsubmit = (event) => {
+  event.preventDefault();
+  // ⚙ Values
+  const valor = inputValor.value;
+  const optionTitular = select.value;
+  const operacaoEscolhida = operacao.value;
+
+  //⚙ Looping para encontrar options selecionadas e execultar operações
+  arrayContas.forEach((conta, i) => {
+    if (
+      conta.titular === optionTitular &&
+      operacaoEscolhida === "depositar" &&
+      validarValor(valor, i)
+    ) {
+      conta.saldo += parseFloat(valor);
+      return console.log(conta.saldo);
+    } else if (
+      conta.titular === optionTitular &&
+      operacaoEscolhida === "sacar" &&
+      validarValor(valor, i)
+    ) {
+      conta.saldo -= parseFloat(valor);
+      return console.log(conta.saldo);
+    }
+  });
+});
+
+//⚙ Consoles logs

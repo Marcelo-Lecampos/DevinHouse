27244aad601241ecc8aeac850ced357ce1b98a11
Marcelo Leão <marcelo.lecampos@gmail.com>
2022-07-30 15:52:38 -0300

    Exercício 5

--- semana 5/exercicios 3-10/script.js
+++ semana 5/exercicios 3-10/script.js
@@ -1,24 +1,34 @@
-
-class Produto{
-    constructor(nome, preco, emEstoque, quantidade){
-        this.nome = nome
-        this.preco = preco
-        this.emEstoque = emEstoque
-        this.quantidade = quantidade
-    }
+class Produto {
+  constructor(nome, preco, emEstoque, quantidade) {
+    this.nome = nome;
+    this.preco = preco;
+    this.emEstoque = emEstoque;
+    this.quantidade = quantidade;
+  }
 }
-// Exercício 4
+// Exercício 5
 
-// Crie uma classe Pedido, com as propriedades numeroPedido, dataPedido, estaPago, listaProdutos e nomeCliente.
+// Dentro da classe Pedido, adicione um método adicionarProduto, que recebe como argumento um objeto do tipo Produto ( caso deseje, você pode realizar uma validação utilizando o operador instanceof).
 
-// Utilize o método construtor para que, a cada instancia dessa classe, um número do pedido e o nome do cliente seja informado, a dataPedido seja a data atual ( new Date( ).toLocaleDateString( ) ), o estaPago seja falso e a listaProdutos seja um array vazio.
+// Este método deve adicionar (push) à propriedade listaProdutos o produto passado como argumento.
 
-class Pedido{
-    constructor(numeroPedido, nomeCliente){
-        this.numeroPedido = numeroPedido
-        this.nomeCliente = nomeCliente
-        this.dataPedido = new Date().toLocaleDateString()
-        this.estaPago = false
-        this.listaProdutos = []
-    }
+class Pedido {
+  constructor(numeroPedido, nomeCliente, produto) {
+    this.numeroPedido = numeroPedido;
+    this.nomeCliente = nomeCliente;
+    this.dataPedido = new Date().toLocaleDateString();
+    this.estaPago = false;
+    this.listaProdutos = [];
+    this.adicionarProduto(produto);
+  }
+  adicionarProduto(produto) {
+    this.listaProdutos.push(produto);
+  }
 }
+
+
+const teste= new Pedido(1, "João", "queijo");
+
+console.log(teste instanceof Pedido)
+
+console.log(teste); 
